8e459eeb2af6f73dd3a2e47a04d0a36e2fb3c3bc
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-13 14:42:17 +0300

    bmiCalculator as command-line arguments

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,12 +1,51 @@
-const calculateBmi = (height: number, weight: number): string => {
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const classifyBmi = (height: number, weight: number): void => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
 
-  if (bmi < 18.5) return "Underweight";
-  if (bmi < 25) return "Normal range";
-  if (bmi < 30) return "Overweight";
+  let res: string;
 
-  return "Obese";
+  if (bmi < 18.5) {
+    res = "Underweight";
+  } else if (bmi < 25) {
+    res = "Normal range";
+  } else if (bmi < 30) {
+    res = "Overweight";
+  } else {
+    res = "Obese";
+  }
+
+  console.log(res);
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseArguments(process.argv);
+
+  classifyBmi(height, weight);
+} catch (error: unknown) {
+  let errorMessage = "Failed to calculate BMI.";
+
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+
+  console.log(errorMessage);
+}
