8e0d0842f3c4a3f8712b0e1c3389e16dcdfc597a
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-24 14:22:56 +0300

    add api/ and port

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
