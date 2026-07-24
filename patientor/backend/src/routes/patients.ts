f8c52a79b392dd0a1f5a82d3ec5588e924d69bfb
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-24 18:18:15 +0300

    add patient service

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import patientService from '../services/patientService.ts';
+
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all patients!');
+  const data = patientService.getPatients();
+  res.send(data);
 });
 
 router.post('/', (_req, res) => {
---
+++ patientor/backend/src/services/patientService.ts
@@ -0,0 +1,14 @@
+import patientData from '../../data/patients.ts';
+
+const getPatients = () => {
+  return patientData;
+};
+
+const addPatient = () => {
+  return null;
+};
+
+export default {
+  getPatients,
+  addPatient
+};
