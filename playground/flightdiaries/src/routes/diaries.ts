81d8c0fcf9b25d8221bf1e8986b95dea99625e45
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-24 15:19:28 +0300

    add service

--- playground/flightdiaries/src/routes/diaries.ts
+++ playground/flightdiaries/src/routes/diaries.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 
+import diaryService from '../services/diaryService.ts';
+
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all diaries!');
+  const data = diaryService.getEntries()
+  res.send(data);
 });
 
 router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+  res.send("add a new diary");
 });
 
 export default router;
