8fb5105f8793e6abfcf7adadaa3d23d34fe01042
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-24 17:55:42 +0300

    exports the typed data

--- patientor/backend/src/services/diagnosService.ts
+++ patientor/backend/src/services/diagnosService.ts
@@ -1,8 +1,6 @@
-import diagnosData from '../../data/diagnoses.ts';
+import diagnoses from '../../data/diagnoses.ts';
 
 import type { Diagnosis } from '../types.ts';
-
-const diagnoses: Diagnosis[] = diagnosData as Diagnosis[];
 
 const getEntries = (): Diagnosis[] => {
   return diagnoses;
