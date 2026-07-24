08349f2cd396dc44c7f6298a0dcf450c7816a6e7
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-24 15:04:19 +0300

    add diaries router

--- playground/flightdiaries/src/index.ts
+++ playground/flightdiaries/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import diaryRouter from './routes/diaries.ts';
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,8 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
