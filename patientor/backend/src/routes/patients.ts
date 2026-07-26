325826e1920a99de019d40e016af1a1890dc6a15
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-26 14:59:58 +0300

    POST with addPatient function

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,6 +1,6 @@
 import express, { type Response } from 'express';
 
-import type { NonSensitivePatient } from '../types.ts';
+import type { NonSensitivePatient, Patient, NewPatient } from '../types.ts';
 import patientService from '../services/patientService.ts';
 
 const router = express.Router();
@@ -9,8 +9,19 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.json(patientService.getNonSensitiveEntries());
 });
 
-router.post('/', (_req, res: Response<string>) => {
-  res.send('Saving a patient!');
+router.post('/', (req, res: Response<Patient | string>) => {
+  try {
+    const newPatient: NewPatient = req.body;
+    const addedPatient = patientService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
