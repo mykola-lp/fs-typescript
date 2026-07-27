680be0e48b432654e5e9aa8f5436b9de705fd257
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-27 12:31:01 +0300

    upd parseNewPatientEntry

--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -1,30 +1,7 @@
-import { Gender, type NewPatient } from './types.ts';
+import { type NewPatientEntry, NewPatientEntrySchema } from './types.ts';
 
-import { z } from 'zod';
-
-const parseNewPatientEntry = (object: unknown): NewPatient => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('Incorrect or missing data');
-  }
-
-  if (
-    'name' in object &&
-    'dateOfBirth' in object &&
-    'ssn' in object &&
-    'gender' in object &&
-    'occupation' in object
-  ) {
-    const newEntry: NewPatient = {
-      name: z.string().parse(object.name),
-      dateOfBirth: z.iso.date().parse(object.dateOfBirth),
-      ssn: z.string().parse(object.ssn),
-      gender: z.enum(Gender).parse(object.gender),
-      occupation: z.string().parse(object.occupation),
-    };
-    return newEntry;
-  }
-
-  throw new Error('Incorrect data: some fields are missing');
+const parseNewPatientEntry = (object: unknown): NewPatientEntry => {
+  return NewPatientEntrySchema.parse(object);
 };
 
 export default parseNewPatientEntry;
