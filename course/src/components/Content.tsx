4d88db8fe62d32eb97eb8bea978ba9bfdc183281
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 12:01:08 +0300

    render course parts by kind with typed switch

--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -1,7 +1,25 @@
-type CoursePart = {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
-};
+}
+
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
 
 interface ContentProps {
   courseParts: CoursePart[];
@@ -10,11 +28,41 @@ interface ContentProps {
 const Content = ({ courseParts }: ContentProps) => {
   return (
     <>
-      {courseParts.map((part) => (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
-      ))}
+      {courseParts.map((part) => {
+        switch (part.kind) {
+          case "basic":
+            return (
+              <p key={part.name}>
+                {part.name} {part.exerciseCount}
+                <br />
+                {part.description}
+              </p>
+            );
+
+          case "group":
+            return (
+              <p key={part.name}>
+                {part.name} {part.exerciseCount}
+                <br />
+                project exercises {part.groupProjectCount}
+              </p>
+            );
+
+          case "background":
+            return (
+              <p key={part.name}>
+                {part.name} {part.exerciseCount}
+                <br />
+                {part.description}
+                <br />
+                submit to {part.backgroundMaterial}
+              </p>
+            );
+
+          default:
+            return null;
+        }
+      })}
     </>
   );
 };
