c3b512a3a6290c00d785a1cf48f1cd60e04b018c
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 10:20:29 +0300

    verbose syntax

--- playground/my-app-name/src/main.tsx
+++ playground/my-app-name/src/main.tsx
@@ -1,12 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import type { ReactElement } from 'react'
 
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps): ReactElement => {
-  return <h1>Hello, {props.name}</h1>;
+const Welcome = ({ name }: { name: string }): ReactElement => {
+  return <h1>Hello, {name}</h1>;
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
