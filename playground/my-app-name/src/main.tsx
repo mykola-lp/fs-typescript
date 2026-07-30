2484b0630cc6d7a90cda32da4e667f2335da3a17
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 10:18:26 +0300

    function type

--- playground/my-app-name/src/main.tsx
+++ playground/my-app-name/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import type { ReactElement } from 'react'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = (props: WelcomeProps): ReactElement => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Welcome name="Sarah" />
 )
