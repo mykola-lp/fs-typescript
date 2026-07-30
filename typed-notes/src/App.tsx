ae71f6641abc9e19cce09bdde67dd6cafe48aad7
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 14:47:03 +0300

    add some data to the state

--- typed-notes/src/App.tsx
+++ typed-notes/src/App.tsx
@@ -3,10 +3,24 @@ import { useState } from 'react';
 import type { Note } from './types'
 
 const App = () => {
-  const [newNote, setNewNote] = useState('');
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: '1',
+      content: 'testing'
+    }
+  ]);
 
-  return <h1>Hello</h1>;
+  const [newNote, setNewNote] = useState('');
+
+  return (
+    <div>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id}>{note.content}</li>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
