00a35fee7b8565b859c46fa8ae423e8937e7aa5f
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 11:38:17 +0300

    define a type for course parts

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -2,6 +2,30 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
 
+interface CoursePartBasic {
+  name: string;
+  exerciseCount: number;
+  description: string;
+  kind: "basic"
+}
+
+interface CoursePartGroup {
+  name: string;
+  exerciseCount: number;
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground {
+  name: string;
+  exerciseCount: number;
+  description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
 const App = () => {
   const courseName = "Half Stack application development";
 
