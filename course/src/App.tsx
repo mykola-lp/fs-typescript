8d582bb362706fada076d15232c2356abe48b941
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 11:31:59 +0300

    add new structure for courseParts

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -4,19 +4,29 @@ import Total from './components/Total';
 
 const App = () => {
   const courseName = "Half Stack application development";
+
   const courseParts = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types"
+    },
   ];
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
