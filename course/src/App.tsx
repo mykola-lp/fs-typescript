1f7d9e03dd3bff6f5263fa5ea27bf884e65f01ea
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 12:04:36 +0300

    extract CoursePart types into shared types.ts

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -2,28 +2,7 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
 
-interface CoursePartBase {
-  name: string;
-  exerciseCount: number;
-}
-
-interface CoursePartBasic extends CoursePartBase {
-  description: string;
-  kind: "basic"
-}
-
-interface CoursePartGroup extends CoursePartBase {
-  groupProjectCount: number;
-  kind: "group"
-}
-
-interface CoursePartBackground extends CoursePartBase {
-  description: string;
-  backgroundMaterial: string;
-  kind: "background"
-}
-
-type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+import type { CoursePart } from './types';
 
 const App = () => {
   const courseName = "Half Stack application development";
--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -1,25 +1,4 @@
-interface CoursePartBase {
-  name: string;
-  exerciseCount: number;
-}
-
-interface CoursePartBasic extends CoursePartBase {
-  description: string;
-  kind: "basic";
-}
-
-interface CoursePartGroup extends CoursePartBase {
-  groupProjectCount: number;
-  kind: "group";
-}
-
-interface CoursePartBackground extends CoursePartBase {
-  description: string;
-  backgroundMaterial: string;
-  kind: "background";
-}
-
-type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+import type { CoursePart } from '../types';
 
 interface ContentProps {
   courseParts: CoursePart[];
