19e671fb24fc9cfcac4948954e0a785e5b7bfc79
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 14:44:50 +0300

    set Note type to the useState

--- typed-notes/src/App.tsx
+++ typed-notes/src/App.tsx
@@ -4,7 +4,7 @@ import type { Note } from './types'
 
 const App = () => {
   const [newNote, setNewNote] = useState('');
-  const [notes, setNotes] = useState<string[]>([]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   return <h1>Hello</h1>;
 }
