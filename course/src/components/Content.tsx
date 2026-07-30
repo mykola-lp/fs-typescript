714ec89554565d78f401c895ca9bd28a84294a0b
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-30 12:40:32 +0300

    extract Part component from switch logic

--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -1,56 +1,17 @@
+import Part from './Part';
+
 import type { CoursePart } from '../types';
 
 interface ContentProps {
   courseParts: CoursePart[];
 }
 
-/**
- * Helper function for exhaustive type checking
- */
-const assertNever = (value: never): never => {
-  throw new Error(
-    `Unhandled discriminated union member: ${JSON.stringify(value)}`
-  );
-};
-
 const Content = ({ courseParts }: ContentProps) => {
   return (
     <>
-      {courseParts.map((part) => {
-        switch (part.kind) {
-          case "basic":
-            return (
-              <p key={part.name}>
-                {part.name} {part.exerciseCount}
-                <br />
-                {part.description}
-              </p>
-            );
-
-          case "group":
-            return (
-              <p key={part.name}>
-                {part.name} {part.exerciseCount}
-                <br />
-                project exercises {part.groupProjectCount}
-              </p>
-            );
-
-          case "background":
-            return (
-              <p key={part.name}>
-                {part.name} {part.exerciseCount}
-                <br />
-                {part.description}
-                <br />
-                submit to {part.backgroundMaterial}
-              </p>
-            );
-
-          default:
-            return assertNever(part);
-        }
-      })}
+      {courseParts.map((part) => (
+        <Part key={part.name} coursePart={part} />
+      ))}
     </>
   );
 };
---
+++ course/src/components/Part.tsx
@@ -0,0 +1,52 @@
+import type { CoursePart } from '../types';
+
+interface PartProps {
+  coursePart: CoursePart;
+}
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const Part = ({ coursePart }: PartProps) => {
+  switch (coursePart.kind) {
+    case "basic":
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount}
+          <br />
+          {coursePart.description}
+        </p>
+      );
+
+    case "group":
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount}
+          <br />
+          project exercises {coursePart.groupProjectCount}
+        </p>
+      );
+
+    case "background":
+      return (
+        <p>
+          {coursePart.name} {coursePart.exerciseCount}
+          <br />
+          {coursePart.description}
+          <br />
+          submit to {coursePart.backgroundMaterial}
+        </p>
+      );
+
+    default:
+      return assertNever(coursePart);
+  }
+};
+
+export default Part;
