720450320d383bda6e140aafa4f73ece7f4b3766
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-31 15:58:45 +0300

    add formatDate

--- flightdiaries/frontend/src/components/DiaryList.tsx
+++ flightdiaries/frontend/src/components/DiaryList.tsx
@@ -10,7 +10,7 @@ const DiaryList = ({ diaries }: DiaryListProps) => {
       {diaries.map((diary) => (
         <li key={diary.id}>
           <div className="diary-entry-header">
-            <strong className="diary-date">{diary.date}</strong>
+            <strong className="diary-date">{formatDate(diary.date)}</strong>
             <span className="diary-weather">{diary.weather}</span>
             <span className="diary-visibility">{diary.visibility}</span>
           </div>
@@ -21,5 +21,13 @@ const DiaryList = ({ diaries }: DiaryListProps) => {
     </ul>
   );
 };
+
+function formatDate(isoDate: string): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(new Date(isoDate));
+}
 
 export default DiaryList;
