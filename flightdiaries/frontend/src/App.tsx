742e45733e4efb4b5ec57c27f091378e7f59040d
mykola-lp <mykola.sukhorab@gmail.com>
2026-07-31 09:11:36 +0300

    add service to the App

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react';
+
+import type { NonSensitiveDiaryEntry } from './types';
+
+import diaryService from './services/diaries';
+
 function App() {
-  return <h1>Hello</h1>
+  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+
+  useEffect(() => {
+    diaryService
+      .getAll()
+      .then(data => {
+        setDiaries(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Flight diaries</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
