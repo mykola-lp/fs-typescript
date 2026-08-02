560b4d08c4ba091bb1fe62fbe19f59db7a018c7f
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-02 12:11:17 +0300

    add PatientPage component

---
+++ patientor/frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography } from '@mui/material';
+
+import type { Patient } from "../../types";
+import patientService from "../../services/patients";
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      if (!id) return;
+      const fetchedPatient = await patientService.getById(id);
+      setPatient(fetchedPatient);
+    };
+
+    void fetchPatient();
+  }, [id]);
+
+  if (!patient) {
+    return <Typography>Loading...</Typography>;
+  }
+
+  return (
+    <div>
+      <Typography variant="h5" sx={{ marginTop: "1em" }}>
+        {patient.name}
+      </Typography>
+
+      <Typography>gender: {patient.gender}</Typography>
+
+      <Typography>ssn: {patient.ssn}</Typography>
+
+      <Typography>occupation: {patient.occupation}</Typography>
+    </div>
+  );
+};
+
+export default PatientPage;
