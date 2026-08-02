50df1188401c778438ae2759c20ae71040efaa7c
mykola-lp <mykola.sukhorab@gmail.com>
2026-08-02 15:47:52 +0300

    define a special Omit-like function

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -77,3 +77,9 @@ export type Entry =
   | HospitalEntry
   | OccupationalHealthcareEntry
   | HealthCheckEntry;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+// Define Entry without the 'id' property
+type EntryWithoutId = UnionOmit<Entry, 'id'>;
